ea4bf1f4d5119ef6ad73c010067d27ba8afdc1e5
Saurav <sp.gharti@gmail.com>
2023-09-27 17:04:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tst-proxy-connector-17",
+  url: "https://tst-proxy-connector-17.botics.co",
+  api: "https://tst-proxy-connector-17.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
